9b5ff53d62c6b6227168977f5b5734674a269dbc
somekh <levsus-saat@mail.ru>
2024-09-25 17:27:14 +0300

    first_one

---
+++ app.py
@@ -0,0 +1,19 @@
+import streamlit as st
+import pandas as pd
+from scipy import stats
+
+st.title("Анализ продаж")
+
+# Загрузка данных
+df = pd.read_csv("stats.csv", encoding='cp1251')
+
+df = df.set_axis(['work_days', 'age', 'gender'], axis=1)
+df['gender'] = df['gender'].replace({'М': 1, 'Ж': 0})
+# Визуализация данных
+st.line_chart(df["work_days"])
+
+# Фильтрация данных
+date_filter = st.slider("Выберите период", min_value=df["age"].min(), max_value=df["age"].max())
+filtered_df = df[df["age"] <= date_filter]
+st.line_chart(filtered_df["work_days"])
+
