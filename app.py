bf6e88b7847fbf7a8fcfdd8aa941f359dfb869c7
Somekh <105787250+Somekh@users.noreply.github.com>
2024-09-25 16:17:36 +0000

    333

--- app.py
+++ app.py
@@ -2,18 +2,18 @@ import streamlit as st
 import pandas as pd
 from scipy import stats
 
-st.title("Анализ продаж")
+# st.title("Анализ продаж")
 
-# Загрузка данных
-df = pd.read_csv("stats.csv", encoding='cp1251')
+# # Загрузка данных
+# df = pd.read_csv("stats.csv", encoding='cp1251')
 
-df = df.set_axis(['work_days', 'age', 'gender'], axis=1)
-df['gender'] = df['gender'].replace({'М': 1, 'Ж': 0})
-# Визуализация данных
-st.line_chart(df["work_days"])
+# df = df.set_axis(['work_days', 'age', 'gender'], axis=1)
+# df['gender'] = df['gender'].replace({'М': 1, 'Ж': 0})
+# # Визуализация данных
+# st.line_chart(df["work_days"])
 
-# Фильтрация данных
-date_filter = st.slider("Выберите период", min_value=df["age"].min(), max_value=df["age"].max())
-filtered_df = df[df["age"] <= date_filter]
-st.line_chart(filtered_df["work_days"])
+# # Фильтрация данных
+# date_filter = st.slider("Выберите период", min_value=df["age"].min(), max_value=df["age"].max())
+# filtered_df = df[df["age"] <= date_filter]
+# st.line_chart(filtered_df["work_days"])
 
