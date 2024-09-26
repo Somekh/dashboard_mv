4d2fe881b921823b262852938b823a118385be12
Somekh <105787250+Somekh@users.noreply.github.com>
2024-09-26 07:54:10 +0000

    next

--- app.py
+++ app.py
@@ -2,15 +2,17 @@ import streamlit as st
 import pandas as pd
 from scipy import stats
 
-# st.title("Анализ продаж")
+st.title("Анализ продаж")
 
-# # Загрузка данных
-# df = pd.read_csv("stats.csv", encoding='cp1251')
+# Загрузка данных
+df = pd.read_csv("stats.csv", encoding='cp1251')
 
-# df = df.set_axis(['work_days', 'age', 'gender'], axis=1)
-# df['gender'] = df['gender'].replace({'М': 1, 'Ж': 0})
-# # Визуализация данных
-# st.line_chart(df["work_days"])
+df = df.set_axis(['work_days', 'age', 'gender'], axis=1)
+df['gender'] = df['gender'].replace({'М': 1, 'Ж': 0})
+df.to_parquet('ttr.parquet')
+# Визуализация данных
+st.header('рабочие дни', divider='gray')
+st.line_chart(df["work_days"])
 
 # # Фильтрация данных
 # date_filter = st.slider("Выберите период", min_value=df["age"].min(), max_value=df["age"].max())
