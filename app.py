a61afc63290b3707a38179089bbabac9eef020c5
Somekh <105787250+Somekh@users.noreply.github.com>
2024-09-26 10:51:57 +0000

    5

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ df = df.set_axis(['work_days', 'age', 'gender'], axis=1)
 df['gender'] = df['gender'].replace({'М': 1, 'Ж': 0})
 df.to_parquet('ttr.parquet')
 # Визуализация данных
-age = st.number_input("Укажите уровень стат. значимости:", min_value=0.01, max_value=0.2, value=0.05)
+age = st.number_input("Укажите уровень стат. значимости:", value=0.05)
 st.header('рабочие дни', divider='gray')
 st.line_chart(df["work_days"])
 
@@ -21,4 +21,4 @@ st.line_chart(df["work_days"])
 # filtered_df = df[df["age"] <= date_filter]
 # st.line_chart(filtered_df["work_days"])
 
-age = st.number_input("Введите ваш возраст:", min_value=0, max_value=120, value=30)
+# age = st.number_input("Введите ваш возраст:", min_value=0, max_value=120, value=30)
